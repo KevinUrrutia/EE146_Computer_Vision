d7c2fe7f4a3eb7e92b2513898836437c6a023828
KevinUrrutia <kurru006@ucr.edu>
2022-01-04 18:32:30 -0800

    added lab1

---
+++ Lab1/lab1_part1.py
@@ -0,0 +1,10 @@
+'''
+Problem 1: Become familiar with image display related functions in matlab. Display a
+gray scale image, color image and binary image. Find the number of rows, columns and
+the number of bytes/pixel. Convert a color image to a gray scale image. What
+transformation is used here?
+'''
+
+import cv2 as cv
+
+#get an image from opencv
